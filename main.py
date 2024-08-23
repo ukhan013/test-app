20b43b56ce0d8f812b3e99f18767cdc82930823d
ukhan013 <khanursha41@gmail.com>
2024-08-24 01:00:01 +0500

    initial

---
+++ main.py
@@ -0,0 +1,20 @@
+import flet as ft
+
+def main(page: ft.Page):
+
+    page.title = "NavigationBar Example"
+    page.navigation_bar = ft.NavigationBar(
+        destinations=[
+            ft.NavigationBarDestination(icon=ft.icons.EXPLORE, label="Explore"),
+            ft.NavigationBarDestination(icon=ft.icons.COMMUTE, label="Commute"),
+            ft.NavigationBarDestination(
+                icon=ft.icons.BOOKMARK_BORDER,
+                selected_icon=ft.icons.BOOKMARK,
+                label="Explore",
+            ),
+        ]
+        
+    )
+    page.add(ft.Text("hellow its my 1st app"))
+
+ft.app(target=main)
